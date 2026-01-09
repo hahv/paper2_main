96925f41b7ed9bad570608cb69f89b5975f4790b
hahv <hoangvanhauit@gmail.com>
2026-01-09 17:27:52 +0900

    update

--- temp.py
+++ temp.py
@@ -1,13 +1,13 @@
-from halib import *
-from src.utils import *
+from halib import * # noqa: F403
+from tap import *
 
-pkg_cls = {
-    "src.methods": ["no_temp_method", "temp_Baseline_TPT_method", "temp_method"],
-    "src.metrics": ["csv_metric_src"],
-    "src.results": ["csv_rs_proc", "video_base_rs_proc", "video_rs_fgmask_proc"],
-}
+class CustomArgs(Tap):
+    # --- Basic Types ---
+    arg_str: str = "MyProject"  # Description acts as help text
+def main():
+    # Parse arguments
+    args = CustomArgs().parse_args()
+    print(f"Argument value: {args.arg_str}")
 
-for pkg_name, file_names in pkg_cls.items():
-    for file_name in file_names:
-        cls = get_cls_in_pkg(pkg_name, fileName_or_fileNameAndClsName=file_name)
-        pprint(cls)
+if __name__ == "__main__":
+    main()
