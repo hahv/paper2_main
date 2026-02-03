217dab2d4c782fdbdb3133af378e243cb929c7fa
hahv <hoangvanhauit@gmail.com>
2026-02-03 13:54:49 +0900

    [data_split] add code to copy files

--- zrun/run_vdb_split_csv.py
+++ zrun/run_vdb_split_csv.py
@@ -1,16 +1,14 @@
 from halib import *
-
 from halib.common.common import seed_everything
-import os
-from tap import Tap
-from sklearn.model_selection import train_test_split
 from halib.filetype import csvfile
+from sklearn.model_selection import train_test_split
+from tap import Tap
 from typing import Literal
 
 
 class DBSplitCSVArgs(Tap):
     video_dir: str = "/mnt/d/zdataset_paper2/build_video_dataset/my_firesmoke_indoor/firesmoke"  # Path to your video folder
-    task: Literal["mk_csv", "split_by_csv"] = (
+    task: Literal["mk_csv", "split_by_csv", "copy_files"] = (
         "split_by_csv"  # Set a default value if desired  # Set a default value if desired
     )
     out_dir: str = "/mnt/e/zDatasets/paper2_datasets"  # Output directory
@@ -20,11 +18,19 @@ class DBSplitCSVArgs(Tap):
 
 
 META_DATA_CSV = "video_metadata"
+FILE_NAME_SEP = "__"
+TABLE_NAME = "video_meta"
+SET_NAMES = ["test", "val"]
 
 
 def get_meta_csv_file_path(video_dir, out_dir):
     video_dir_name = fs.get_dir_name(video_dir)
     return os.path.join(out_dir, f"{video_dir_name}_{META_DATA_CSV}.csv")
+
+
+def get_split_csv_file_path(video_dir, out_dir, split_name):
+    video_dir_name = fs.get_dir_name(video_dir)
+    return os.path.join(out_dir, f"{video_dir_name}_{split_name}_split.csv")
 
 
 def get_video_dir_meta_csv(video_dir, out_dir):
@@ -45,8 +51,6 @@ def get_video_dir_meta_csv(video_dir, out_dir):
     )
     # the new csv will have columns: video_path, data_source, category
     dfmk = csvfile.DFCreator()
-    FILE_NAME_SEP = "__"
-    TABLE_NAME = "video_meta"
 
     dfmk.create_table(
         table_name=TABLE_NAME, columns=["video_path", "data_source", "category"]
@@ -126,13 +130,9 @@ def split_by_csv(csv_path, video_dir, out_dir, val_size=0.3, seed=42, add_time=T
     print(val_df[["data_source", "category"]].value_counts())
 
     df_ls = [test_df, val_df]
-    set_names = ["test", "val"]
 
-    for df, set_name in zip(df_ls, set_names):
-        out_csv_file = os.path.join(
-            out_dir,
-            f"{fs.get_dir_name(video_dir)}_{set_name}_split.csv",
-        )
+    for df, set_name in zip(df_ls, SET_NAMES):
+        out_csv_file = get_split_csv_file_path(video_dir, out_dir, set_name)
         df.to_csv(
             out_csv_file,
             index=False,
@@ -143,11 +143,48 @@ def split_by_csv(csv_path, video_dir, out_dir, val_size=0.3, seed=42, add_time=T
         pprint_local_path(out_csv_file, get_wins_path=True)
 
 
+def copy_files(video_dir, out_dir):
+    csv_files = [
+        get_split_csv_file_path(video_dir, out_dir, split) for split in SET_NAMES
+    ]
+    assert all([os.path.exists(csv_file) for csv_file in csv_files]), (
+        "Split CSV files not found. Please run split_by_csv first."
+    )
+    video_dir_name = fs.get_dir_name(video_dir)
+    for csv_file, set_name in zip(csv_files, SET_NAMES):
+        df = pd.read_csv(csv_file, sep=";", encoding="utf-8")
+        out_set_dir = os.path.join(out_dir, f"{video_dir_name}_{set_name}")
+        os.makedirs(out_set_dir, exist_ok=True)
+        for _, row in tqdm(
+            df.iterrows(), total=len(df), desc=f"Copying files to {set_name}..."
+        ):
+            # ! for video file
+            video_src_path = row["video_path"]
+            assert os.path.exists(video_src_path), (
+                f"Video file not found: {video_src_path}"
+            )
+            file_name = os.path.basename(video_src_path)
+            video_dst_path = os.path.join(out_set_dir, file_name)
+
+            # ! for annotation file
+            parent_dir = os.path.dirname(video_src_path)
+            anno_file_name = f"{fs.get_file_name(video_src_path, split_file_ext=True)[0]}__labels.csv"
+            anno_src_path = os.path.join(parent_dir, anno_file_name)
+            anno_dst_path = os.path.join(out_set_dir, anno_file_name)
+            assert os.path.exists(anno_src_path), (
+                f"Annotation file not found: {anno_src_path}"
+            )
+            # Do actualy copying if no assertion errors
+            fs.copy_file(video_src_path, video_dst_path)
+            fs.copy_file(anno_src_path, anno_dst_path)
+
+
 def main():
     # Parse arguments
     args = DBSplitCSVArgs().parse_args()
     # ! to ensure reproducibility
     seed_everything(args.seed)
+    console.rule(f"[bold red] Task: {args.task} [/bold red]")
     if args.task == "mk_csv":
         get_video_dir_meta_csv(args.video_dir, args.out_dir)
     elif args.task == "split_by_csv":
@@ -160,6 +197,8 @@ def main():
             seed=args.seed,
             add_time=args.add_time,
         )
+    elif args.task == "copy_files":
+        copy_files(args.video_dir, args.out_dir)
     else:
         raise ValueError(f"Unknown task: {args.task}")
 
