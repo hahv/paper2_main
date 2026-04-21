6ca3722aadd9c8b43dfc6bfb3001b9e5553433c7
hahv <hoangvanhauit@gmail.com>
2026-04-21 11:31:56 +0900

    [add eager mode] periodically check

--- zrun_case.sh
+++ zrun_case.sh
@@ -1,3 +1,10 @@
+# ! MUST DO: config/zruns/select_opt_methods.yaml
+# !- choose the METHODs to RUN or to OPTIMIZE
+
+## ! PRECOMPUTED:
+#  ! TEST set: zout/zruns/MainPC__ds_UFireIndoorTest__mt_no_temp_method__af4b0d32a3d2__20260330.205208
+#  ! VAL set: zout/zruns/MainPC__ds_UFireIndoorVal__mt_no_temp_method__af4b0d32a3d2__20260327.162459
+
 # python ./zbin/run_multi.py \
 #     --base_yaml "config/zruns/run_base.yaml" \
 #     --sweep_yaml "config/zruns/select_ds_ufire_02.yaml"
@@ -10,4 +17,10 @@
 #     --sweep_yaml "config/zruns/select_ds_ufire_val.yaml" \
 #     -pc "zout/zruns/MainPC__ds_UFireIndoorVal__mt_no_temp_method__af4b0d32a3d2__20260327.162459"
 
-./zrun.sh -p val -opt
+# ./zrun.sh -p val -opt
+
+# ! RUN:  AccMotionDet (Eager Mode) on test set for optimized config
+python ./zbin/run_multi.py \
+    --base_yaml "config/zruns/run_base.yaml" \
+    --sweep_yaml "config/zruns/select_ds_ufire_test.yaml" \
+    -pc "zout/zruns/MainPC__ds_UFireIndoorTest__mt_no_temp_method__af4b0d32a3d2__20260330.205208"
