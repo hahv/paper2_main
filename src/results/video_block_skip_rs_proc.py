086032fa03badb0a9cb3c2f5583719c6d8e6ab3b
hahv <hoangvanhauit@gmail.com>
2026-08-05 17:44:30 +0900

    clean up

--- src/results/video_block_skip_rs_proc.py
+++ src/results/video_block_skip_rs_proc.py
@@ -10,8 +10,6 @@ from src.results.viz.infer_rs_renderer import InferRsRenderer
 from src.results.viz.grid_renderer import GridRenderer
 from src.results.video_infer_rs_proc import VideoInferRsProc
 from src.results.viz.block_motion_only_renderer import BlockMontionOnlyRenderer
-from src.results.viz.block_rule_based_renderer import BlockRuleBasedRenderer
-
 
 class VideoBlockSkipRsProc(VideoInferRsProc):
     def __init__(self, cfg: Config):
@@ -22,10 +20,10 @@ class VideoBlockSkipRsProc(VideoInferRsProc):
     # ! Normal cases: Frame size of out video = input frame size
     def calc_frame_size(self, video_path: str, **kwargs) -> tuple:
         if self.renderer_context == "original_frame":
-            return kwargs["frame_size"]  # ty:ignore[invalid-return-type]
+            return kwargs["frame_size"]
         else:
             original_fsize = kwargs.get("frame_size")
-            method_cfg = self.cfg.methodCfg.extra_cfgs.get("skip_proc").get("params")  # ty:ignore[possibly-missing-attribute]
+            method_cfg = self.cfg.methodCfg.extra_cfgs.get("skip_proc").get("params")  # ty:ignore[unresolved-attribute]
             scale_factor = method_cfg.get("scale_factor")
             block_size_orig = method_cfg.get("block_size_orig")
             return BaseBlockSkipProc.get_skip_proc_frame_size(
@@ -41,15 +39,13 @@ class VideoBlockSkipRsProc(VideoInferRsProc):
     def get_block_extra_dict(self) -> Dict[str, Any]:
         return {"viz_in_fgmask": False}
 
-    def get_custom_renderer(self) -> List[BaseRenderer]:
-        method_cfg = self.cfg.methodCfg.extra_cfgs.get("skip_proc")  # ty:ignore[possibly-missing-attribute]
+    def get_custom_renderer(self) -> List[BaseRenderer]:  # ty:ignore[invalid-return-type]
+        method_cfg = self.cfg.methodCfg.extra_cfgs.get("skip_proc")  # ty:ignore[unresolved-attribute]
         # ! decide which renderer to use based on method config (which method is used)
         is_motion_only = "motion_only" in method_cfg["name"]  # ty:ignore[not-subscriptable]
         # ! we alway render in original frame size
         if is_motion_only:
             return [BlockMontionOnlyRenderer(context=self.renderer_context)]
-        else:
-            return [BlockRuleBasedRenderer(context=self.renderer_context)]
 
     def prepare_pipelines_list(self, video_path: str, fps: float, frame_size: tuple):
         pipeline_ls = []
